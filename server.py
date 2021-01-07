922e321bedd5b5a50107934d2a7563da8e01f9b1
Ioan Zicu <ioan.zicu94@gmail.com>
2021-01-07 06:08:13 +0100

    contact form

--- server.py
+++ server.py
@@ -1,6 +1,4 @@
-import os
-from flask import Flask, render_template
-from flask.helpers import send_from_directory
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 print(__name__)
 
@@ -13,3 +11,13 @@ def home():
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return redirect('thankyou.html')
+    else:
+        return 'Something went wrong. Try again!'
